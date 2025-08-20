2e3078a79c908c0cf88b29097a896a61ee8ce9e0
neojarvis <techadmin@iamneo.ai>
2025-08-20 18:21:38 +0000

    Report

--- Project/src/test/java/runner/LoginTest.java
+++ Project/src/test/java/runner/LoginTest.java
@@ -6,8 +6,12 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
 import pages.LoginPage;
 import utils.Base;
+import utils.Reporter;
 import utils.Screenshot;
 import utils.WebDriverHelper;
 
@@ -15,17 +19,20 @@ public class LoginTest extends Base{
 
     private LoginPage loginPage;
     private WebDriverHelper helper;
+    private ExtentReports extent;
+    private ExtentTest test;
     @BeforeMethod
     public void setUp(){
         openBrowser();
         this.helper = new WebDriverHelper(driver);
         loginPage = new LoginPage(driver, helper);
-
+        extent = Reporter.generateExtentReport();
     }
 
 
     @Test(priority = 1)
     public void loginTest(){
+        test = extent.createTest("Signin / registrartin on Agronetto");
         loginPage.login();
         Set<String> parentHandles = driver.getWindowHandles();
         loginPage.clickImg();
@@ -35,6 +42,7 @@ public class LoginTest extends Base{
         loginPage.clickOnVerify();
         loginPage.clickOnContactSeller();
         Screenshot.captureScreenShot("agronetto_screenshot");
+        test.pass("Agronetto test passed");
     }
 
     @Test(priority = 2)
